0b8497860049128049ed569324ba0649c565c930
Ivan Hofer <ivan.hofer@outlook.com>
2023-01-09 15:17:18 +0100

    x

--- inlang.config.js
+++ inlang.config.js
@@ -2,29 +2,14 @@
  * @type {import("@inlang/core/config").InitializeConfig}
  */
 export async function initializeConfig(env) {
+  const plugin = await env.$import(
+    "https://cdn.jsdelivr.net/gh/ivanhofer/inlang-plugin-typesafe-i18n/dist/index.js"
+  )
+
   return {
     referenceLanguage: "en",
     languages: ["en", "de"],
-    readResources: async (args) => {
-      console.log('read', args);
-      const x = await loadLocale(env.$import, args.config.referenceLanguage)
-      console.log(x);
-      return []
-    },
-    writeResources: (args) => {
-      console.log('write', args);
-    }
-  };
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-const loadLocale = async ($import, locale) => {
-  const esbuild = await $import('https://cdn.skypack.dev/-/esbuild@v0.16.16-QlnhVgB2NGGPL1heU74X/dist=es2019,mode=imports/optimized/esbuild.js')
-  console.log(111, esbuild);
-  const x = await esbuild.build({ entryPoints: [`./src/i18n/${locale}/index.ts`], outfile: './out.js' })
-  console.log(1, x);
-  const module = await $import(/* vite-ignore */ `./out.js`);
-  console.log(11, module);
-  return module.default
+    readResources: async (args) => plugin.readResources({ ...args, ...env, pluginConfig }),
+    writeResources: (args) => plugin.writeResources({ ...args, ...env, pluginConfig }),
+  }
 }
