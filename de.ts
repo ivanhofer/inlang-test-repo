b3724a7f439c4c61b585755360193a0d733dcebd
Ivan Hofer <ivan.hofer@outlook.com>
2023-01-11 09:50:29 +0100

    fix

--- de.ts
+++
@@ -1,7 +0,0 @@
-import type { Translation } from './src/i18n/i18n-types'
-
-const de: Translation = {
-   "HI": "Hallo {name}! Bitte hinterlasse einen Stern, wenn dir das Projekt gef�llt: https://github.com/ivanhofer/typesafe-i18n"
-}
-
-export default de
