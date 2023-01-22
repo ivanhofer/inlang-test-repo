77806361163f951d1f18621db06500d49883b285
Hofer Ivan <ivan.hofer@outlook.com>
2023-01-22 08:42:51 +0100

    update to latest config

--- inlang.config.js
+++ inlang.config.js
@@ -1,15 +1,21 @@
 /**
  * @type {import("@inlang/core/config").InitializeConfig}
  */
-export async function initializeConfig(env) {
+export async function defineConfig(env) {
+  // initialize the plugin
   const plugin = await env.$import(
     "https://cdn.jsdelivr.net/gh/ivanhofer/inlang-plugin-typesafe-i18n/dist/index.js"
   )
 
+  // get the locale information from `typesafe-i18n`
+  const { base, locales } = await getLocaleInformation(env.$fs)
+
   return {
-    referenceLanguage: "en",
-    languages: ["en", "de"],
-    readResources: async (args) => plugin.readResources({ ...args, ...env }),
-    writeResources: (args) => plugin.writeResources({ ...args, ...env }),
+    referenceLanguage: base,
+    languages: locales,
+    readResources: (args) =>
+      plugin.readResources({ ...args, ...env }),
+    writeResources: (args) =>
+      plugin.writeResources({ ...args, ...env }),
   }
 }
